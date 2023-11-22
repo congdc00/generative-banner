57e90725758591e7b839a8ac6966cc5a197204b0
congdc00 <dinhchicongf9@gmail.com>
2023-11-22 16:22:01 +0700

    Fix Pix Art not working

--- core/pix_art.py
+++ core/pix_art.py
@@ -4,7 +4,7 @@ class PixArt_Model():
 
     def __init__(self, device, img_size):
         self.model = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16).to(device)
-        # self.model.enable_model_cpu_offload()
+        self.model.enable_model_cpu_offload()
     
     def gen(self, prompt, negative_promt="", steps = 65):
         image = self.model(prompt = prompt).images[0]
